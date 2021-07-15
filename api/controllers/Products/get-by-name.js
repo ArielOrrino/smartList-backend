441ddb39b346bf62824d7240baf56152cf2909af
Ariel Orrino <ariel.orrino@kognitiv.com>
2021-07-15 00:46:54 -0300

    added user Agent as header

--- api/controllers/Products/get-by-name.js
+++ api/controllers/Products/get-by-name.js
@@ -8,7 +8,8 @@ module.exports = {
     const { productName, sucursales: sucursalesString, distancia } = query;
     const limit = 50;
     const headers = {
-      'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
+      'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
     };
     const sucursales = JSON.parse(sucursalesString);
     const axiosParams = {
--- api/controllers/Products/get-sucursales.js
+++ api/controllers/Products/get-sucursales.js
@@ -8,7 +8,8 @@ module.exports = {
     const { lat, lng } = query;
     const limit = 3000;
     const headers = {
-      'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
+      'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
     };
     const axiosParams = {
       url: UrlsService.getSucursales(),
