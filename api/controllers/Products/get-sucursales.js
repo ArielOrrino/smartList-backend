2bdeb5963365365850e6f9c60bef82043017e5f0
Ariel Orrino <ariel.orrino@kognitiv.com>
2021-07-15 23:11:31 -0300

    volviendo todo a como estaba...

--- api/controllers/Products/get-sucursales.js
+++ api/controllers/Products/get-sucursales.js
@@ -1,17 +1,13 @@
 module.exports = {
 
-
   async fn(inputs, exits, env) {
     const {req, res} = env;
     const { query } = req;
     const { reqId } = res.options;
     const { lat, lng } = query;
     const limit = 3000;
-    delete req.headers.host;
     const headers = {
-      ...req.headers,
       'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
     };
     const axiosParams = {
       url: UrlsService.getSucursales(),
@@ -25,14 +21,6 @@ module.exports = {
     };
     console.log('AxiosParams get-sucursales: ', axiosParams);
     const [err, data] = await ToService.promiseToAsync(AxiosService.get(axiosParams));
-    console.log('\x1b[31m');
-    console.log('**************************************************************');
-    console.log('\x1b[37m');
-    console.log('offers err: ', err);
-    console.log('offers data: ', data);
-    console.log('\x1b[31m');
-    console.log('**************************************************************');
-    console.log('\x1b[37m');
     if (err) {
       return res.ok(err);
     }
