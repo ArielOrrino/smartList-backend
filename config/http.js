98391cb1e34617e417e176a082f9bfa892eab5da
Ariel Orrino <ariel.orrino@kognitiv.com>
2021-07-15 23:18:16 -0300

    Update http.js

--- config/http.js
+++ config/http.js
@@ -1,60 +1,94 @@
+/*eslint-disable*/
 /**
  * HTTP Server Settings
  * (sails.config.http)
  *
  * Configuration for the underlying HTTP server in Sails.
- * (for additional recommended settings, see `config/env/production.js`)
+ * Only applies to HTTP requests (not WebSockets)
  *
  * For more information on configuration, check out:
- * https://sailsjs.com/config/http
+ * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.http.html
  */
 
-module.exports.http = {
+ module.exports.http = {
 
   /****************************************************************************
   *                                                                           *
-  * Sails/Express middleware to run for every HTTP request.                   *
-  * (Only applies to HTTP requests -- not virtual WebSocket requests.)        *
-  *                                                                           *
-  * https://sailsjs.com/documentation/concepts/middleware                     *
+  * Express middleware to use for every Sails request. To add custom          *
+  * middleware to the mix, add a function to the middleware config object and *
+  * add its key to the "order" array. The $custom key is reserved for         *
+  * backwards-compatibility with Sails v0.9.x apps that use the               *
+  * `customMiddleware` config option.                                         *
   *                                                                           *
   ****************************************************************************/
 
   middleware: {
 
-    /***************************************************************************
-    *                                                                          *
-    * The order in which middleware should be run for HTTP requests.           *
-    * (This Sails app's routes are handled by the "router" middleware below.)  *
-    *                                                                          *
-    ***************************************************************************/
+  /***************************************************************************
+  *                                                                          *
+  * The order in which middleware should be run for HTTP request. (the Sails *
+  * router is invoked by the "router" middleware below.)                     *
+  *                                                                          *
+  ***************************************************************************/
 
     // order: [
+    //   'startRequestTimer',
     //   'cookieParser',
     //   'session',
+    //   'myRequestLogger',
     //   'bodyParser',
+    //   'handleBodyParserError',
     //   'compress',
+    //   'methodOverride',
     //   'poweredBy',
+    //   '$custom',
     //   'router',
     //   'www',
     //   'favicon',
+    //   '404',
+    //   '500'
     // ],
 
+  /****************************************************************************
+  *                                                                           *
+  * Example custom middleware; logs each request to the console.              *
+  *                                                                           *
+  ****************************************************************************/
 
-    /***************************************************************************
-    *                                                                          *
-    * The body parser that will handle incoming multipart HTTP requests.       *
-    *                                                                          *
-    * https://sailsjs.com/config/http#?customizing-the-body-parser             *
-    *                                                                          *
-    ***************************************************************************/
+    // myRequestLogger: function (req, res, next) {
+    //     console.log("Requested :: ", req.method, req.url);
+    //     return next();
+    // }
 
-    // bodyParser: (function _configureBodyParser(){
-    //   var skipper = require('skipper');
-    //   var middlewareFn = skipper({ strict: true });
-    //   return middlewareFn;
-    // })(),
+
+  /***************************************************************************
+  *                                                                          *
+  * The body parser that will handle incoming multipart HTTP requests. By    *
+  * default,Sails uses [skipper](http://github.com/balderdashy/skipper). See *
+  * https://github.com/expressjs/body-parser for other options. Note that    *
+  * Sails uses an internal instance of Skipper by default; to override it    *
+  * and specify more options, make sure to "npm install                      *
+  * skipper@for-sails-0.12 --save" in your app first. You can also specify a *
+  * different body parser or a custom function with req, res and next        *
+  * parameters (just like any other middleware function).                    *
+  *                                                                          *
+  ***************************************************************************/
+
+
+    // bodyParser: require('skipper')({strict: true})
 
   },
+
   trustProxy: true,
+
+  /***************************************************************************
+  *                                                                          *
+  * The number of milliseconds to cache static assets in production.         *
+  * These are any flat files like images, scripts, styleshseets, etc.        *
+  * that are served by the static middleware.  By default, these files       *
+  * are served from `.tmp/public`, a hidden folder compiled by Grunt.        *
+  *                                                                          *
+  ***************************************************************************/
+
+  // cache: 31557600000
 };
