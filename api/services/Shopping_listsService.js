6fdeaef0416462ec2c418eab8db1a81825e781b0
NahuelCad <32886091+NahuelCad@users.noreply.github.com>
2021-07-26 21:35:48 -0300

    Create and Get - ShoppingLists
    Files Creates
    Shopping_listsService
    Controller/Shopping_lists

---
+++ api/services/Shopping_listsService.js
@@ -0,0 +1,29 @@
+const moment = require('moment');
+const Shopping_lists = require('../models/Shopping_lists');
+
+module.exports = {
+  findShopping_list({id}) {
+    return ShoppingLists.findOne({id});
+  },
+
+  getAllShopping_lists() {
+    return ShoppingLists.find();
+  },
+
+  async createShopping_list({createdBy}) {
+    let listCreated;
+    try {
+      listCreated = await ShoppingLists.create({createdBy}).fetch();
+    } catch (err) {
+      return {
+        message: 'List couldnt be created',
+        error: true,
+        code:500,
+      };
+    }
+    
+    return listCreated;
+
+  },
+
+};
