0a2d4b5c7fc16b3e83d4486b9aede2885b3df5de
Ariel Orrino <ariel.orrino@kognitiv.com>
2021-09-29 22:40:46 -0300

    return lists by creatorId

--- api/controllers/Shopping-lists/remove.js
+++ api/controllers/Shopping-lists/remove.js
@@ -2,11 +2,11 @@ module.exports = {
 
   async fn(inputs, exits, env) {
     const {res,req} = env;
-    const {id} = req.query;
-    let list = await ShoppingListsService.remove({id});
+    const {id, createdBy} = req.query;
+    let list = await ShoppingListsService.remove({id, createdBy});
     if (list && list.error){
       return res.serverError(list);
     }
-    return res.ok();
+    return res.ok(list);
   },
 };
