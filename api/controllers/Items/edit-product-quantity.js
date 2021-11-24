cb27fa8bcf4424966624a75275d48f98628d85dc
Ariel Orrino <ariel.orrino@kognitiv.com>
2021-11-24 16:35:55 -0300

    added controls for bad request

--- api/controllers/Items/edit-product-quantity.js
+++ api/controllers/Items/edit-product-quantity.js
@@ -4,6 +4,9 @@ module.exports = {
     const {res, req} = env;
     const { body } = req;
     const { quantity,id } = body;
+    if (!quantity || !id) {
+      return res.badRequest('Quantity or id is not being sent');
+    }
     const itemEdited = await ItemsService.editProductQuantity({quantity,id});
     if (itemEdited.error){
       return res.serverError(itemEdited);
