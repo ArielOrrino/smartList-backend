8470ab10d058a2faf9be7e68f549e3599ba65aa5
Ariel Orrino <ariel.orrino@kognitiv.com>
2022-11-17 14:30:47 -0300

    trying to find the login issue

--- api/controllers/Shopping-lists/get-by-creator.js
+++ api/controllers/Shopping-lists/get-by-creator.js
@@ -2,12 +2,14 @@ module.exports = {
 
   async fn(inputs, exits, env) {
     const {res,req} = env;
-    const {idCreator} = req.query;
+    const { idCreator } = req.query;
+    console.log('idCreator: ', idCreator);
     let list = await ShoppingListsService.findShoppingListByCreator({idCreator});
     if (list.error){
       return res.serverError(list);
     }
     const sortedLists = _.sortBy(list, 'name');
+    console.log('listas: ', sortedLists);
     return res.ok(sortedLists);
   },
 };
