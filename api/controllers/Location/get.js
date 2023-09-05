ee20d73bacc8d64cb25242598880a676a369d0cd
Ariel Orrino <ariel.orrino@kognitiv.com>
2023-09-05 17:20:41 -0300

    Update get.js

--- api/controllers/Location/get.js
+++ api/controllers/Location/get.js
@@ -1,13 +1,16 @@
 module.exports = {
 
   async fn(inputs, exits, env) {
-    const {req, res} = env;
+    const { req, res } = env;
+    console.log('entro al request /location/get');
     const { query } = req;
     const { reqId } = res.options;
     const { lat, lon } = query;
+    console.log('parametros recibidos lat:%s, lon:%s', lat, lon);
     const headers = {
       'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
     };
+    console.log('preparo el request a la api de geolocalizacion');
 
     const axiosParams = {
       url: UrlsService.getLocation(),
@@ -15,22 +18,12 @@ module.exports = {
       headers,
       params: { lat,lon },
     };
+    console.log('axiosParams: ', axiosParams);
     const [err, data] = await ToService.promiseToAsync(AxiosService.get(axiosParams));
     if (err) {
+      console.debug('err: ', err);
       return res.ok(err);
     }
-    // log as debug the error and the response
-    console.debug('data: ', data.data);
-    console.debug('err: ', err);
-
-    console.info('data: ', data.data);
-    console.info('err: ', err);
-
-    console.warn('data: ', data.data);
-    console.warn('err: ', err);
-
-    console.error('data: ', data.data);
-    console.error('err: ', err);
 
     const {municipio, provincia} = data.data.ubicacion;
     const {nombre: MuniNombre} =municipio;
