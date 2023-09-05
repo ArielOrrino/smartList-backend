7962be4dc53a35b42fc5be8f7d3aace1824e70d0
Ariel Orrino <ariel.orrino@kognitiv.com>
2023-09-05 17:52:09 -0300

    Update HelperService.js

--- api/services/HelperService.js
+++ api/services/HelperService.js
@@ -1,5 +1,5 @@
 module.exports = {
   getUserAgent() {
-    return {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'};
+    return {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'};
   }
 };
