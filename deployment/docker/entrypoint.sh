611e7d26a8296201812c2e8e4f8b78aaa60b7670
Ariel Orrino <ariel.orrino@kognitiv.com>
2024-01-18 15:20:38 -0300

    changes on dockerfile

--- deployment/docker/entrypoint.sh
+++ deployment/docker/entrypoint.sh
@@ -4,10 +4,15 @@ set -e
 # Run command with node if the first argument contains a "-" or is not a system command. The last
 # part inside the "{}" is a workaround for the following bug in ash/dash:
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=874264
-if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ] || { [ -f "${1}" ] && ! [ -x "${1}" ]; }; then
-  set -- node "$@"
-fi
+#if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ] || { [ -f "${1}" ] && ! [ -x "${1}" ]; }; then
+#  set -- node "$@"
+#fi
 
-exec "$@"
+#exec "$@"
 
-exec npm run start:prod
+npm install
+
+npm run start:prod
+
+
+
