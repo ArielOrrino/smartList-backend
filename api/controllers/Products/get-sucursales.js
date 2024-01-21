d1795a0851f20acd792299c7d08d0ceb3f05e604
Ariel Orrino <ariel.orrino@kognitiv.com>
2024-01-20 22:51:18 -0300

    Update get-sucursales.js

--- api/controllers/Products/get-sucursales.js
+++ api/controllers/Products/get-sucursales.js
@@ -21,10 +21,18 @@ module.exports = {
       };
       const [err, data] = await ToService.promiseToAsync(AxiosService.get(axiosParams));
       if (err || data.status !== 200 || !data.data.sucursales) {
-        const errorResponse = err || data.data;
-        return res.ok(errorResponse);
+        const dataWithError = {
+          errorMessage: 'Estamos actualizando la lista de precios, intente nuevamente en unos minutos.'
+        };
+        return res.ok(dataWithError);
       }
       const sucursales = SucursalesService.filterSucursales(data.data.sucursales);
+      if (sucursales.length === 0) {
+        const dataWithError = {
+          errorMessage: 'No se encontraron supermercados dentro del radio de búsqueda.',
+        };
+        return res.ok(dataWithError);
+      }
       return res.ok(sucursales);
     } catch (error) {
       return res.serverError(error);
