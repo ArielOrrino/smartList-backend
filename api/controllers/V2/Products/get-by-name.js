c16019909bc1766f241192dc9cab2458a259c2fb
Ariel Orrino <ariel.orrino@kognitiv.com>
2024-01-31 16:53:21 -0300

    changes on searching algorithm

--- api/controllers/V2/Products/get-by-name.js
+++ api/controllers/V2/Products/get-by-name.js
@@ -1,6 +1,5 @@
+const AVOID_WORDS = ['a', 'e', 'i', 'o', 'u', 'la', 'las', 'lo', 'los', 'le', 'les'];
 module.exports = {
-
-
   async fn(inputs, exits, env) {
     const { req, res } = env;
     try {
@@ -10,12 +9,21 @@ module.exports = {
       const limit = 50;
       const headers = HeadersService.createHeaders();
       const arraySucursales = stringSucursales.split(',');
-      const axiosParams = {
+      const splited_name = productName.split(' ');
+      let new_search = splited_name[0];
+      while (splited_name.length > 0 && AVOID_WORDS.includes(new_search.toLowerCase())) {
+        splited_name.shift();
+        new_search = splited_name[0];
+      }
+      if (splited_name.length === 0) {
+        return res.ok({ productos: [], total: 0 });
+      }
+      let axiosParams = {
         url: UrlsService.getProductsByName(),
         reqId,
         headers,
         params: {
-          string: productName,
+          string: new_search,
           // eslint-disable-next-line camelcase
           array_sucursales: arraySucursales,
           offset: 0,
@@ -27,9 +35,17 @@ module.exports = {
       if (err) {
         return res.serverError(err);
       }
+      productos = ProductsService.translateProductsByName(data.data.productos);
+      total = productos.length;
+      splited_name.shift();
+      splited_name.forEach(name => {
+        productos = productos.filter((producto) => producto.nombre.toLowerCase().includes((name + ' ').toLowerCase()));
+        total = productos.length;
+      })
+
       const response = {
-        productos: ProductsService.translateProductsByName(data.data.productos),
-        total: data.data.productos.length
+        productos,
+        total
       };
       return res.ok(response);
     } catch (error) {
